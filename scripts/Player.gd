4f9d25f5d6b402ff2b52df3d845cb49c321067cf
Jacpowerz <136922755+Jacpowerz@users.noreply.github.com>
2023-08-04 17:29:53 -0500

    finished second episode

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed : float = 200.0
-@export var double_jump_velocity : float = -150
+
 
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var animation_tree : AnimationTree = $AnimationTree
@@ -9,10 +9,7 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped : bool = false
-var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
-var was_in_air : bool = false
 
 func _ready():
 	animation_tree.active = true
@@ -21,25 +18,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		was_in_air = true
-	else:
-		has_double_jumped = false
-		
-		if was_in_air == true:
-			land()
-			
-		was_in_air = false
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			# Normal jump from floor
-#			jump()
-			pass
-		elif not has_double_jumped:
-			# Double jump in air
-			double_jump()
-			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -51,10 +29,10 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-	update_animation()
+	update_animation_parameters()
 	update_facing_direction()
 	
-func update_animation():
+func update_animation_parameters():
 	animation_tree.set("parameters/Move/blend_position", direction.x)
 
 func update_facing_direction():
@@ -62,13 +40,3 @@ func update_facing_direction():
 		sprite.flip_h = false
 	elif direction.x < 0:
 		sprite.flip_h = true
-	
-func double_jump():
-	velocity.y = double_jump_velocity
-#	animated_sprite.play("jump_double")
-	animation_locked = true
-	has_double_jumped = true
-
-func land():
-#	animated_sprite.play("jump_end")
-	animation_locked = true
--- scripts/states/State.gd
+++ scripts/states/State.gd
@@ -6,6 +6,16 @@ class_name State
 
 var character : CharacterBody2D
 var next_state : State = null
+var playback : AnimationNodeStateMachinePlayback
 
-func state_input(event):
+func state_process(_delta):
+	pass
+
+func state_input(_event):
+	pass
+	
+func on_enter():
+	pass
+
+func on_exit():
 	pass
